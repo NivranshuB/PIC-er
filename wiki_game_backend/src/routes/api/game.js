b8daf652ad922ab4bae272450d9ddad5b17438ef
NivranshuB <nbos433@aucklanduni.ac.nz>
2022-05-04 21:43:21 +1200

    Changed request for 'next 5 images' from PUT to GET

--- wiki_game_backend/src/routes/api/game.js
+++ wiki_game_backend/src/routes/api/game.js
@@ -12,31 +12,30 @@ const HTTP_NO_CONTENT = 204;
 
 const router = express.Router();
 
-//Create a new game when you get a POST request
-router.post('/game', async (req, res) => {
+//Retrieve start and target images for a new game when you get a GET request
+router.get('/', (req, res) => {
 
     //Retrieve two random image objects and their associated tags list
     //Set one of the image as the start image and the other as the target image
-    const newGameImages = await createGame;//unimplemented
+    const newGameImages = createGame();
 
     //Send the start image and the target image in the response payload
-    res.status(HTTP_CREATED)//unimplemented
-        .json(newGameImages);
+    res.json(newGameImages);
 })
 
-//Upon getting a PUT request send back 5 new images based on the game logic
-router.put('/game', async (req, res) => {
+//Upon getting a GET request to /ongoing send back 5 new images based on the game logic
+router.get('/ongoing', (req, res) => {
+    console.log("GET /game/ongoing called on server")
 
     //Analyse the image url that the user selected
 
     //Make changes to the game depending on their game progress
 
     //Retrieve 5 image urls from the database (with one of the images being the progressing image)
-    const nextImageSet = await nextLevelImages();//unimplemented
+    const nextImagesSet = nextLevelImages();
 
     //Send the url of the 5 images back to the client in the response payload
-    res.status(HTTP_CREATED)//unimplemented
-    .json(nextImageSet);
+    res.json(nextImagesSet);
 })
 
 export default router;
