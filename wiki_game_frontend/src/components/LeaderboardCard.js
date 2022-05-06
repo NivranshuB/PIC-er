3d309000a5195782ba6e5ec36443386bbf3bd2e9
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-07 00:40:43 +1200

    add LeaderboardCard component

---
+++ wiki_game_frontend/src/components/LeaderboardCard.js
@@ -0,0 +1,25 @@
+import { Box, Center, Flex } from "@chakra-ui/react";
+
+const LeaderboardCard = (props) => {
+
+    const { rank, name, clicks, time } = props;
+
+    return (
+            <Flex bg='lighterBackground' borderRadius='8px' p='4px'>
+                <Center flex='1'>
+                    {rank}
+                </Center>
+                <Box flex='2'>
+                    {name}
+                </Box>
+                <Center flex='1'>
+                    {clicks}
+                </Center>
+                <Center flex='1'>
+                    {time}
+                </Center>
+            </Flex>
+    )
+}
+
+export default LeaderboardCard;
--- wiki_game_frontend/src/pages/HomePage.js
+++ wiki_game_frontend/src/pages/HomePage.js
@@ -1,9 +1,11 @@
+import LeaderboardCard from "../components/LeaderboardCard";
 
 
 const HomePage = () => {
     return (
         <div>
             inside navbar
+            <LeaderboardCard/>
         </div>
     )
 }
