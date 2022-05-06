2585d7b917ec0091701867ae1c97177fde778d8c
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-06 20:56:49 +1200

    add initial navbar components

--- wiki_game_frontend/src/pages/NavbarPage.js
+++ wiki_game_frontend/src/pages/NavbarPage.js
@@ -1,11 +1,20 @@
-import { Flex } from "@chakra-ui/react";
+import { Button, ButtonGroup, Center, Flex, Spacer } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
 const NavbarPage = () => {
     return (
         <div>
             <Flex color='accent' backgroundColor='lighterBackground'>
+                <Center pl='4'>
                 PIC-er
+                </Center>
+                
+                <Spacer />
+                <ButtonGroup>
+                    <Button>Sign Up</Button>
+                    <Button>Login</Button>
+                </ButtonGroup>
+
             </Flex>
             {/* Renders children pages */}
             <Outlet />
