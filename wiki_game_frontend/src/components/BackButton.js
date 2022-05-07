d462102ec2d48ce21c37cf713ad07edfb2a80ef6
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-07 15:53:06 +1200

    add chakra ui icons and arrow to back button

--- wiki_game_frontend/src/components/BackButton.js
+++ wiki_game_frontend/src/components/BackButton.js
@@ -1,12 +1,13 @@
 import { Button } from "@chakra-ui/react"
 import { useNavigate } from "react-router-dom";
+import {ArrowBackIcon} from "@chakra-ui/icons";
 
 const BackButton = () => {
 
     const navigate = useNavigate();
 
     return (
-        <Button onClick={() => navigate(-1)}>Back</Button>
+        <Button leftIcon={<ArrowBackIcon/>} onClick={() => navigate(-1)}>Back</Button>
     )
 }
 
