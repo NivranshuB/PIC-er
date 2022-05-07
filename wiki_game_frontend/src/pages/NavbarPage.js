5a0f1f357fc0dbeef4560d2a8248acdb63b76cc1
Joe Cheong <j.cheong@hotmail.com>
2022-05-07 17:14:03 +1200

    Adding auth0 redirection

--- wiki_game_frontend/src/pages/NavbarPage.js
+++ wiki_game_frontend/src/pages/NavbarPage.js
@@ -2,9 +2,12 @@ import { Button, ButtonGroup, Center, Flex, Spacer, useDisclosure } from "@chakr
 import { Link, Outlet } from "react-router-dom";
 import LoginModal from "../components/LoginModal";
 import SignupModal from "../components/SignupModal";
+import { useAuth0 } from "@auth0/auth0-react";
+
+
 
 const NavbarPage = () => {
-
+    const { user, loginWithRedirect, logout, isAuthenticated } = useAuth0();
     const {isOpen, onOpen, onClose} = useDisclosure();
     const {isOpen: isOpenSignup, onOpen: onOpenSignup, onClose: onCloseSignup} = useDisclosure();
 
@@ -20,9 +23,13 @@ const NavbarPage = () => {
 
                 <Spacer />
                 <ButtonGroup>
-                    <Button variant='borderlessWhite' onClick={onOpenSignup}>Sign Up</Button>
+                    <Button variant='borderlessWhite' onClick={() => loginWithRedirect({screen_hint: 'signup'})}>Sign Up</Button>
                     <SignupModal isOpen={isOpenSignup} onClose={onCloseSignup}/>
-                    <Button variant='borderless' onClick={onOpen}>Login</Button>
+                    {isAuthenticated 
+                    ? <span><Button variant='borderless' onClick={() => logout({returnTo: window.location.origin})}>Logout</Button><h2>{user.email}</h2></span>
+                    : <Button variant='borderless' onClick={() => loginWithRedirect()}>Login</Button>}
+                    
+                    
                     <LoginModal isOpen={isOpen} onClose={onClose}/>
                 </ButtonGroup>
 
