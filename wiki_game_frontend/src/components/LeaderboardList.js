419c1a55ca62c3528e7ea500212f320f7deaa500
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-07 21:26:22 +1200

    feat sort leaderboard by clicks asc and desc

--- wiki_game_frontend/src/components/LeaderboardList.js
+++ wiki_game_frontend/src/components/LeaderboardList.js
@@ -1,9 +1,27 @@
-import { Box, Center, Flex, Heading, Spacer } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Heading, Spacer } from "@chakra-ui/react";
+import { useState } from "react";
 import LeaderboardCard from "./LeaderboardCard"
 
 const LeaderboardList = (props) => {
 
     const { items, title } = props;
+
+    const [clicksSorted, setClicksSorted] = useState(true);
+
+    const handleSort = () => {
+        setClicksSorted(!clicksSorted)
+        console.log(clicksSorted)
+
+        if (clicksSorted) {
+            items.sort((a, b) => (a.clicks < b.clicks) ? 1 : -1);
+        } else {
+            // items.sort((a, b) => (a.time.localeCompare(b.time)) ? 1 : -1);
+            items.sort((a, b) => (a.clicks > b.clicks) ? 1 : -1);
+        }
+    }
+
+    console.log(items);
+    
 
     return (
         <Box width='100%' ml='16px' mr='16px'>
@@ -15,22 +33,27 @@ const LeaderboardList = (props) => {
 
             <Flex borderRadius='8px' p='4px' ml='8px' mr='8px' >
                 <Center flex='1' >
-                <Heading size='lg'>#</Heading>
+                    <Heading size='lg'>#</Heading>
                 </Center>
                 <Box flex='2'>
-                <Heading size='lg'>Name</Heading>
+                    <Heading size='lg'>Name</Heading>
                 </Box>
                 <Center flex='1'>
-                <Heading size='lg'>Clicks</Heading>
+                    <Button variant={clicksSorted ? 'borderless' : 'borderlessWhite'} onClick={() => handleSort()}>
+                        <Heading size='lg'>Clicks</Heading>
+                    </Button>
+
                 </Center>
                 <Center flex='1'>
-                <Heading size='lg'>Time</Heading>
+                    <Button variant={clicksSorted ? 'borderlessWhite' : 'borderless'} onClick={() => handleSort()}>
+                        <Heading size='lg'>Time</Heading>
+                    </Button>
                 </Center>
             </Flex>
 
-            {items.map((item) => {
+            {items.map((item, index) => {
                 return (
-                    <LeaderboardCard rank={item.rank} name={item.name} clicks={item.clicks} time={item.time} />
+                    <LeaderboardCard rank={index + 1} name={item.name} clicks={item.clicks} time={item.time} />
                 )
             })}
 
--- wiki_game_frontend/src/pages/LeaderboardPage.js
+++ wiki_game_frontend/src/pages/LeaderboardPage.js
@@ -5,8 +5,10 @@ import LeaderboardList from "../components/LeaderboardList";
 const LeaderboardPage = () => {
 
     const personalHighScores = [
-        { rank: 1, name: "person1", clicks: 3, time: "3:04" },
-        { rank: 2, name: "person2", clicks: 3, time: "7:07" },
+        { rank: 1, name: "person1", clicks: 10, time: "a" },
+        { rank: 2, name: "person2", clicks: 11, time: "f" },
+        { rank: 2, name: "person3", clicks: 15, time: "b" },
+        { rank: 2, name: "person4", clicks: 1, time: "z" },
     ];
 
     const globalHighScores = [
