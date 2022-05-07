a869ce6a5d6fddf5e24a3941fbd4907bbeb7e4dc
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-07 15:49:36 +1200

    add BackButton component Reusable back button to reduce code duplication

--- wiki_game_frontend/src/pages/LeaderboardPage.js
+++ wiki_game_frontend/src/pages/LeaderboardPage.js
@@ -1,13 +1,10 @@
-import { Button } from "@chakra-ui/react";
-import { useNavigate } from "react-router-dom";
+import BackButton from "../components/BackButton";
 
 const LeaderboardPage = () => {
 
-    const navigate = useNavigate();
-
     return (
         <div>
-            <Button onClick={() => navigate(-1)}>Back</Button>
+            <BackButton />
             Leaderboard
         </div>
     )
