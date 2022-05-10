a10e18f64a1f6ac5586cf4751c65e30cb3969a05
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-10 14:30:45 +1200

    add startGame api call to /game

--- wiki_game_frontend/src/services/api.js
+++ wiki_game_frontend/src/services/api.js
@@ -1,5 +1,15 @@
 import axios from 'axios';
 
+export const startGame = async () => {
+    axios.get('/game')
+        .then((response) => {
+            return response.data;
+        })
+        .catch(error => {
+            throw new Error(error.message);
+        })
+}
+
 export const getLeaderboard = async () => {
     axios.get('/leaderboard')
         .then((response) => {
@@ -12,7 +22,7 @@ export const getLeaderboard = async () => {
 
 export const getPersonalLeaderboard = async (id) => {
     axios.get('/leaderboard/' + id)
-        .then(response => {
+        .then((response) => {
             return response.data;
         })
         .catch(error => {
