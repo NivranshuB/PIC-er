911ba12bbd7914f3ad07e0a8030db09c925bd7e4
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-12 17:51:23 +1200

    feat initial layout of images on game page

--- wiki_game_frontend/src/pages/GamePage.js
+++ wiki_game_frontend/src/pages/GamePage.js
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
+import { Box, Button, Center, Flex, Heading, HStack, Image, Spacer, Stack, Text } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import BackButton from "../components/BackButton";
 
@@ -16,12 +16,80 @@ const GamePage = () => {
 
     return (
         <div>
-            <BackButton />
-
             <Flex>
+                <BackButton />
+                <Flex p='24px' width='100%' position='fixed' justifyContent='right'>
+                    <Box alignSelf='center'>
+                        <Heading textAlign='center'>
+                            Target Image
+                        </Heading>
+                        <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" boxSize='300px' fit='contain' />
+                    </Box>
+                </Flex>
+
+            </Flex>
+
+            <Flex direction='column'>
+                <Flex position='fixed' width='20%' textAlign='center' p='16px'>
+                    <Heading size='md'>
+                        Select the image below that best links you to the target image
+                    </Heading>
+                </Flex>
+
+                <Flex alignSelf='center' direction='column'>
+                    <Box>
+                        <Center>
+                            <Box>
+                                <Heading textAlign='center'>
+                                    Current Image
+                                </Heading>
+                                <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" boxSize='300px' fit='contain' />
+                            </Box>
+                        </Center>
+                    </Box>
+
+                    <Box align='center'>
+                        <Box width='8px' bg='accent' height='100px' />
+                        <Box width='65%' bg='accent' height='8px' />
+                        <HStack justify='center' width='66.7%'>
+                            <Box>
+                                <Box width='8px' bg='accent' height='100px' />
+                                <Box borderColor='accent' borderWidth='20px' borderBottomColor='transparent' borderLeftColor='transparent' borderRightColor='transparent' />
+                            </Box>
+                            <Spacer />
+                            <Box>
+                                <Box width='8px' bg='accent' height='100px' />
+                                <Box borderColor='accent' borderWidth='20px' borderBottomColor='transparent' borderLeftColor='transparent' borderRightColor='transparent' />
+                            </Box>
+                            <Spacer />
+                            <Box>
+                                <Box width='8px' bg='accent' height='100px' />
+                                <Box borderColor='accent' borderWidth='20px' borderBottomColor='transparent' borderLeftColor='transparent' borderRightColor='transparent' />
+                            </Box>
+                            <Spacer />
+                            <Box>
+                                <Box width='8px' bg='accent' height='100px' />
+                                <Box borderColor='accent' borderWidth='20px' borderBottomColor='transparent' borderLeftColor='transparent' borderRightColor='transparent' />
+                            </Box>
+                            <Spacer />
+                            <Box>
+                                <Box width='8px' bg='accent' height='100px' />
+                                <Box borderColor='accent' borderWidth='20px' borderBottomColor='transparent' borderLeftColor='transparent' borderRightColor='transparent' />
+                            </Box>
+                        </HStack>
+                    </Box>
+
+                    <HStack spacing='16px' width='100%' justify='center'>
+                        <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" fit='contain' maxWidth='15%' />
+                        <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" maxWidth='15%' fit='contain' />
+                        <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" maxWidth='15%' fit='contain' />
+                        <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" maxWidth='15%' fit='contain' />
+                        <Image src="https://farm8.staticflickr.com/7212/6896667434_2605d9e181_z.jpg" maxWidth='15%' fit='contain' />
+                    </HStack>
+                </Flex>
+
                 <Flex p='24px' position='fixed' bottom='0' width='100%'>
                     <Box alignSelf='center'>
-                        Stuck?
                         <Button>Restart</Button>
                     </Box>
                     <Spacer />
