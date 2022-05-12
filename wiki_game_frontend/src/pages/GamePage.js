f6079ab83d958fe6ed85edce813847512bcb00c2
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-12 15:41:39 +1200

    feat timer Initial timer to be used during the game

--- wiki_game_frontend/src/pages/GamePage.js
+++ wiki_game_frontend/src/pages/GamePage.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import BackButton from "../components/BackButton";
 
 const GamePage = () => {
+
+    const [time, setTime] = useState(0);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setTime(time + 1);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [time]);
+
     return (
         <div>
             <BackButton />
-            game page
+            Time: {time}
         </div>
     )
 }
