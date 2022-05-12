434a1cc64ef0b70b3efd1ff4d89db4efd471c0fa
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-12 15:56:49 +1200

    feat fixed bottom bar with time clicks etc Added fixed bottom bar for game page which displays the restart button, the timer and the number of clicks for the game

--- wiki_game_frontend/src/pages/GamePage.js
+++ wiki_game_frontend/src/pages/GamePage.js
@@ -1,9 +1,11 @@
+import { Box, Button, Flex, Spacer } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import BackButton from "../components/BackButton";
 
 const GamePage = () => {
 
     const [time, setTime] = useState(0);
+    const [clicks, setClicks] = useState(0);
 
     useEffect(() => {
         let interval = setInterval(() => {
@@ -15,7 +17,24 @@ const GamePage = () => {
     return (
         <div>
             <BackButton />
-            Time: {time}
+
+            <Flex>
+                <Flex p='24px' position='fixed' bottom='0' width='100%'>
+                    <Box alignSelf='center'>
+                        Stuck?
+                        <Button>Restart</Button>
+                    </Box>
+                    <Spacer />
+                    <Box alignSelf='center'>
+                        Time: {time}
+                    </Box>
+                    <Spacer />
+                    <Box alignSelf='center'>
+                        Clicks: {clicks}
+                    </Box>
+                </Flex>
+            </Flex>
+
         </div>
     )
 }
