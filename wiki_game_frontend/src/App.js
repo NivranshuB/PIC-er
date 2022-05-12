30ebc12526dacefb260f1e38319db91b7d0e412f
ckar364 <ckar364@aucklanduni.ac.nz>
2022-05-12 22:18:09 +1200

    add initial help page

--- wiki_game_frontend/src/App.js
+++ wiki_game_frontend/src/App.js
@@ -5,6 +5,7 @@ import NavbarPage from './pages/NavbarPage';
 import HomePage from './pages/HomePage';
 import LeaderboardPage from './pages/LeaderboardPage';
 import GameEndPage from './pages/GameEndPage';
+import HelpPage from './pages/HelpPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<NavbarPage />} >
             <Route index element={<HomePage />} />
             <Route path='leaderboard' element={<LeaderboardPage />} />
+            <Route path='help' element={<HelpPage />} />
             <Route path='end' element={<GameEndPage />} />
           </Route>
         </Routes>
