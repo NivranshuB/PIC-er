2bd99f6c73d0fc9e0ebc0820edf9b381fa6019d4
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-14 23:12:05 +1200

    fix NoPersonalScoresCard horizontal scroll bar Does not dispaly horziontal scrollbar when screen is narrower

--- wiki_game_frontend/src/components/NoPersonalScoresCard.js
+++ wiki_game_frontend/src/components/NoPersonalScoresCard.js
@@ -5,10 +5,10 @@ const NoPersonalScoresCard = (props) => {
     const { text } = props;
 
     return (
-        <Flex align='center' width='95%' bg='lighterBackground' borderRadius='8'  ml='16px' mr='16px'>
+        <Flex align='center' bg='lighterBackground' borderRadius='8' ml='16px' mr='16px' mt='8px' >
             <Container>
-                <Stack p='24px'>
-                    <Center>
+                <Stack p='24px' ml='8px' mr='8px' >
+                    <Center >
                         {text}
                     </Center>
                 </Stack>
