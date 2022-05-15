7b55341663bc558693d4df5127e85706033bebbd
Corban Draper <cdra334@aucklanduni.ac.nz>
2022-05-15 19:19:12 +1200

    docs comments for HelpPage and Help components

--- wiki_game_frontend/src/components/HelpNextButton.js
+++ wiki_game_frontend/src/components/HelpNextButton.js
@@ -1,15 +1,22 @@
 import { Button } from "@chakra-ui/react"
 import { totalNumberOfHelpPages } from "../pages/HelpPage";
 
-const HelpNextButton = ({setHelpFirstPage, setHelpLastPage, helpCurrentPageNumber, setHelpCurrentPageNumber, setHelpImageSource}) => {
+/**
+ * Function to move to next image in Help page.
+ * Checks if it is the last page, if true, will display the done button instead of the next button
+ * @param {*} param0 setHelpFirstPage, setHelpLastPage, HelpCurrentPageNumber, setHelpCurrentPageNumber, setHelpImageSource
+ * @returns Next Button
+ */
+const HelpNextButton = ({ setHelpFirstPage, setHelpLastPage, helpCurrentPageNumber, setHelpCurrentPageNumber, setHelpImageSource }) => {
 
     const moveToNextHelpPage = () => {
+        // last next page is the last page, display the done button
         if (helpCurrentPageNumber === (totalNumberOfHelpPages - 1)) {
             setHelpFirstPage(false);
             setHelpLastPage(true)
         } else {
             setHelpFirstPage(false);
-            setHelpLastPage(false);  
+            setHelpLastPage(false);
         }
         setHelpCurrentPageNumber(helpCurrentPageNumber + 1);
         setHelpImageSource('helpImages/helpPage' + (helpCurrentPageNumber + 1) + '.png');
